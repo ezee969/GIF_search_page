e4b62fbfbd171c8b6e6d2a72f975449590138c1a
Ezequiel <ezee.-96@gmail.com>
2022-01-07 10:40:40 -0300

    async - await

---
+++ script2.js
@@ -0,0 +1,44 @@
+class MainPage {
+  init() {
+    this.domCache();
+    this.gifGenerator('cat');
+    this.domBind();
+  }
+
+  domCache() {
+    this.searchBut = document.querySelector('#button');
+    this.inputField = document.querySelector('#input');
+    this.mainCont = document.querySelector('.main-cont');
+    this.errorMesagge = document.querySelector('#error-mesagge');
+  }
+
+  domBind() {
+    this.searchBut.addEventListener('click', () => {
+      this.gifGenerator(this.inputField.value);
+    });
+  }
+
+  async gifGenerator(searchTerm) {
+    try {
+      const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=bxej8jvrToGlsgtSGGfWF4RDDp8eqW11&s=${searchTerm}`, { mode: 'cors' });
+      const data = await response.json();
+      this.errorMesagge.style.display = 'none';
+      this.gifDomRemove();
+      this.img = document.createElement('img');
+      this.img.src = data.data.images.original.url;
+      this.mainCont.appendChild(this.img);
+    } catch (error) {
+      this.gifDomRemove();
+      this.errorMesagge.style.display = 'block';
+    }
+  }
+
+  gifDomRemove() {
+    if (this.img != null) {
+      this.img.remove();
+    }
+  }
+}
+
+const gifPage = new MainPage();
+gifPage.init();
